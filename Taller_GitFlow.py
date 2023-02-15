aff5a7aa1fd328a818e4ab2e4e8c1bf36317c827
juan314david <juan3146466017@gmail.com>
2023-02-15 09:22:58 -0500

    agregar proyecto

---
+++ Taller_GitFlow.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+from openpyxl import load_workbook
+Rut="C:\\Users\\SENA\\Documents\\Taller_GitFlow\\Taller GitFlow Excel.xlsx"
+Rut=r"C:\Users\SENA\Documents\Taller_GitFlow\Taller GitFlow Excel.xlsx"
